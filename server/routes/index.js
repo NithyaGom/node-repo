3358bde7a5b2a8f36aa9711f0e1e884d4e30dc31
NithyaGom <40264479+NithyaGom@users.noreply.github.com>
2018-06-18 15:12:34 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const pg = require('pg');
 const path = require('path');
 //const connectionString = 'postgres://postgres:postgres@db.cxw9rddrpw9p.us-east-2.rds.amazonaws.com:5432/postgresdb';
-const connectionString = process.env.DATABASE_URL;
+//const connectionString = process.env.DATABASE_URL;
+const connectionString = 'postgres://postgres:postgres@192.168.100.134:5432/todo;
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(
     __dirname, '..', '..', 'client', 'views', 'index.html'));
